5502da9958f0399cea890efb72d1abd53a52ad99
TatianaYo <yoienko@yandex.ru>
2023-05-04 16:19:03 +0300

    edit filename

--- homework_1/task_1.py
+++ homework_1/task_1.py
@@ -1,21 +1,20 @@
 """
-Доработать тест на питоне из предыдущего задания таким образом,
-чтобы вывод сохранялся построчно в список и в тесте проверялось, что в этом списке есть строки
-VERSION="22.04.1 LTS (Jammy Jellyfish)" и VERSION_CODENAME=jammy.
-Проверка должна выполняться только если код возврата равен 0.
+Написать функцию на Python, которой передаются в качестве параметров команда и текст. Функция должна возвращать True,
+если команда успешно выполнена и текст найден в её выводе и False в противном случае.
+Передаваться должна только одна строка, разбиение вывода использовать не нужно.
 """
 import subprocess
 
 
-if __name__ == '__main__':
-    result = subprocess.run('cat /etc/os-release', shell=True, stdout=subprocess.PIPE, encoding='utf-8')
-    out = result.stdout
-    if result.returncode == 0:
-        res_list = out.split('\n')
-        print(res_list)
-        if 'VERSION="22.04.1 LTS (Jammy Jellyfish)"' in res_list and 'VERSION_CODENAME=jammy' in res_list:
-            print('SUCCESS')
-        else:
-            print('FAIL')
+def command(com: str, text: str) -> bool:
+    result = subprocess.run(com, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    if result.returncode == 0 and text in result.stdout:
+        return True
     else:
-        print('Process finished with exit code != 0')
+        return False
+
+
+if __name__ == '__main__':
+    command1 = 'cat /etc/os-release'
+    string1 = 'jammy'
+    command(command1, string1)
